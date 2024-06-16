11b63a289bbff13fdd0f50829983f1cb932e08e0
Eduardo Pivaral <39424641+Epivaral@users.noreply.github.com>
2024-06-16 01:47:53 +0000

    changes to function and table

--- backend/function_app.py
+++ backend/function_app.py
@@ -1,12 +1,128 @@
 import logging
 import azure.functions as func
 
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
+import requests
+import pyodbc
+
 app = func.FunctionApp()
 
-@app.schedule(schedule="0 */5 * * * *", arg_name="myTimer", run_on_startup=True,
+@app.schedule(schedule="0 0 * * * *", arg_name="myTimer", run_on_startup=True,
               use_monitor=False) 
-def timer_trigger(myTimer: func.TimerRequest) -> None:
+def get_weather_api(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
         logging.info('The timer is past due!')
 
-    logging.info('Python timer trigger function executed. 223366544')
+    try:
+        logging.info('Starting the process to retrieve secrets from Azure Key Vault.')
+
+        # Get the connection string and API key from Azure Key Vault
+        credential = DefaultAzureCredential()
+        secret_client = SecretClient(vault_url="https://climaguatesecrets.vault.azure.net/", credential=credential)
+        
+        connection_string = secret_client.get_secret("connstr").value
+        logging.info('Successfully retrieved the connection string from Azure Key Vault.')
+        
+        apikey = secret_client.get_secret("apikey").value
+        logging.info('Successfully retrieved the API key from Azure Key Vault.')
+
+        # Connect to the SQL database
+        logging.info('Connecting to the SQL database.')
+        conn = pyodbc.connect(connection_string)
+        cursor = conn.cursor()
+        logging.info('Successfully connected to the SQL database.')
+
+        # Query the weather.cities table
+        logging.info('Executing SQL query to fetch city details.')
+        cursor.execute("SELECT CityCode, CityName, Latitude, Longitude FROM weather.cities;")
+        
+        # Fetch all rows from the query
+        rows = cursor.fetchall()
+        logging.info('Successfully fetched city details from the database.')
+
+        # For each city, log the city details and call the weather API
+        for row in rows:
+            city_code = row.CityCode
+            city_name = row.CityName
+            latitude = row.Latitude
+            longitude = row.Longitude
+            logging.info(f"City Code: {city_code}, City Name: {city_name}, Latitude: {latitude}, Longitude: {longitude}")
+
+            # Call the weather API
+            try:
+                api_call = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={apikey}&lang=es"
+
+                response = requests.get(api_call)
+                response.raise_for_status()  # This will raise an HTTPError if the HTTP request returned an unsuccessful status code
+                data = response.json()
+
+                # Store the weather data in the weather.weatherdata table
+
+                coord_lon = data["coord"]["lon"]
+                coord_lat = data["coord"]["lat"]
+                weather_id = data["weather"][0]["id"]
+                weather_main = data["weather"][0]["main"]
+                weather_description = data["weather"][0]["description"]
+                weather_icon = data["weather"][0]["icon"]
+                base = data["base"]
+                main_temp = data["main"]["temp"]
+                main_feels_like = data["main"]["feels_like"]
+                main_pressure = data["main"]["pressure"]
+                main_humidity = data["main"]["humidity"]
+                main_temp_min = data["main"]["temp_min"]
+                main_temp_max = data["main"]["temp_max"]
+                main_sea_level = data["main"].get("sea_level", None)  # Default to None if not present
+                main_grnd_level = data["main"].get("grnd_level", None)  # Default to None if not present
+                visibility = data["visibility"]
+                wind_speed = data["wind"]["speed"]
+                wind_deg = data["wind"]["deg"]
+                wind_gust = data["wind"].get("gust", None)  # Default to None if not present
+                clouds_all = data["clouds"]["all"]
+                rain_1h = data["rain"].get("1h", None)  # Default to None if not present
+                rain_3h = data["rain"].get("3h", None)  # Default to None if not present
+                dt = data["dt"]
+                sys_country = data["sys"]["country"]
+                sys_sunrise = data["sys"]["sunrise"]
+                sys_sunset = data["sys"]["sunset"]
+                timezone = data["timezone"]
+                city_id = data["id"]
+
+                # Insert statement
+                insert_query = '''
+                INSERT INTO weather.WeatherData (
+                    Coord_Lon, Coord_Lat, Weather_Id, Weather_Main, Weather_Description, 
+                    Weather_Icon, Base, Main_Temp, Main_Feels_Like, Main_Pressure, 
+                    Main_Humidity, Main_Temp_Min, Main_Temp_Max, Main_Sea_Level, 
+                    Main_Grnd_Level, Visibility, Wind_Speed, Wind_Deg, Wind_Gust, 
+                    Clouds_All, Rain_1h, Rain_3h, Dt, Sys_Country, Sys_Sunrise, 
+                    Sys_Sunset, Timezone, Id, Name, CityCode
+                ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        '''
+
+                # Insert data
+                cursor.execute(insert_query, (
+                    coord_lon, coord_lat, weather_id, weather_main, weather_description,
+                    weather_icon, base, main_temp, main_feels_like, main_pressure,
+                    main_humidity, main_temp_min, main_temp_max, main_sea_level,
+                    main_grnd_level, visibility, wind_speed, wind_deg, wind_gust,
+                    clouds_all, rain_1h, rain_3h, dt, sys_country, sys_sunrise,
+                    sys_sunset, timezone, city_id, city_name, city_code
+                ))
+
+            except requests.exceptions.RequestException as e:
+                logging.error(f"Failed to get weather data for {city_name}: {e}")
+
+    except pyodbc.Error as e:
+        logging.error(f"Database connection or query error: {str(e)}")
+
+    except Exception as e:
+        logging.error(f"An error occurred: {str(e)}")
+
+    finally:
+        # Always commit and close, even if an error occurs
+        if conn is not None:
+            conn.commit()
+            if cursor is not None:
+                cursor.close()
+            conn.close()
--- database/WeatherData.sql
+++ database/WeatherData.sql
@@ -1,34 +1,36 @@
 -- Create table
 CREATE TABLE weather.WeatherData (
-    Coord_Lon FLOAT, -- Longitude of the location
-    Coord_Lat FLOAT, -- Latitude of the location
-    Weather_Id INT, -- Weather condition id
-    Weather_Main NVARCHAR(50), -- Group of weather parameters (Rain, Snow, Clouds etc.)
-    Weather_Description NVARCHAR(255), -- Weather condition within the group
-    Weather_Icon NVARCHAR(50), -- Weather icon id
-    Base NVARCHAR(50), -- Internal parameter
-    Main_Temp FLOAT, -- Temperature (Kelvin, Celsius, Fahrenheit)
-    Main_Feels_Like FLOAT, -- Temperature accounting for human perception
-    Main_Pressure INT, -- Atmospheric pressure on the sea level, hPa
-    Main_Humidity INT, -- Humidity, %
-    Main_Temp_Min FLOAT, -- Minimum temperature at the moment (Kelvin, Celsius, Fahrenheit)
-    Main_Temp_Max FLOAT, -- Maximum temperature at the moment (Kelvin, Celsius, Fahrenheit)
-    Main_Sea_Level INT, -- Atmospheric pressure on the sea level, hPa
-    Main_Grnd_Level INT, -- Atmospheric pressure on the ground level, hPa
-    Visibility INT, -- Visibility, meters
-    Wind_Speed FLOAT, -- Wind speed (meter/sec, miles/hour)
-    Wind_Deg INT, -- Wind direction, degrees
-    Wind_Gust FLOAT, -- Wind gust (meter/sec, miles/hour)
-    Clouds_All INT, -- Cloudiness, %
-    Rain_1h FLOAT, -- Rain volume for the last 1 hour, mm
-    Rain_3h FLOAT, -- Rain volume for the last 3 hours, mm (if available)
-    Dt BIGINT, -- Time of data calculation, unix, UTC
-    Sys_Country CHAR(2), -- Country code (GB, JP etc.)
-    Sys_Sunrise BIGINT, -- Sunrise time, unix, UTC
-    Sys_Sunset BIGINT, -- Sunset time, unix, UTC
-    Timezone INT, -- Shift in seconds from UTC
-    Id INT, -- City ID
-    Name NVARCHAR(255), -- City name
-    CityCode CHAR(3) NULL,
+    Coord_Lon FLOAT NOT NULL, -- Longitude of the location
+    Coord_Lat FLOAT NOT NULL, -- Latitude of the location
+    Weather_Id INT NULL, -- Weather condition id
+    Weather_Main NVARCHAR(50) NULL, -- Group of weather parameters (Rain, Snow, Clouds etc.)
+    Weather_Description NVARCHAR(255) NULL, -- Weather condition within the group
+    Weather_Icon NVARCHAR(50) NULL, -- Weather icon id
+    Base NVARCHAR(50) NULL, -- Internal parameter
+    Main_Temp FLOAT NULL, -- Temperature (Kelvin, Celsius, Fahrenheit)
+    Main_Feels_Like FLOAT NULL, -- Temperature accounting for human perception
+    Main_Pressure INT NULL, -- Atmospheric pressure on the sea level, hPa
+    Main_Humidity INT NULL, -- Humidity, %
+    Main_Temp_Min FLOAT NULL, -- Minimum temperature at the moment (Kelvin, Celsius, Fahrenheit)
+    Main_Temp_Max FLOAT NULL, -- Maximum temperature at the moment (Kelvin, Celsius, Fahrenheit)
+    Main_Sea_Level INT NULL, -- Atmospheric pressure on the sea level, hPa
+    Main_Grnd_Level INT NULL, -- Atmospheric pressure on the ground level, hPa
+    Visibility INT NULL, -- Visibility, meters
+    Wind_Speed FLOAT NULL, -- Wind speed (meter/sec, miles/hour)
+    Wind_Deg INT NULL, -- Wind direction, degrees
+    Wind_Gust FLOAT NULL, -- Wind gust (meter/sec, miles/hour)
+    Clouds_All INT NULL, -- Cloudiness, %
+    Rain_1h FLOAT NULL, -- Rain volume for the last 1 hour, mm
+    Rain_3h FLOAT NULL, -- Rain volume for the last 3 hours, mm (if available)
+    Dt BIGINT NULL, -- Time of data calculation, unix, UTC
+    Sys_Country CHAR(2) NULL, -- Country code (GB, JP etc.)
+    Sys_Sunrise BIGINT NULL, -- Sunrise time, unix, UTC
+    Sys_Sunset BIGINT NULL, -- Sunset time, unix, UTC
+    Timezone INT NULL, -- Shift in seconds from UTC
+    Id INT NOT NULL, -- City ID
+    Name NVARCHAR(255) NOT NULL, -- City name
+    CityCode CHAR(3) NOT NULL
 );
 GO
+
+
