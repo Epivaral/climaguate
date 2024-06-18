fef821b4bcebc9cc3b7a27bc3f03fbf2b3ce315f
Eduardo Pivaral <39424641+Epivaral@users.noreply.github.com>
2024-06-18 18:48:45 +0000

    every 30 minutes

--- backend/function_app.py
+++ backend/function_app.py
@@ -8,7 +8,7 @@ import pyodbc
 
 app = func.FunctionApp()
 
-@app.schedule(schedule="0 0 * * * *", arg_name="myTimer", run_on_startup=True,
+@app.schedule(schedule="0 */30 * * * *", arg_name="myTimer", run_on_startup=True,
               use_monitor=False) 
 def get_weather_api(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
