6518d5c02413e71eca504516cb9149d9bd495aea
Eduardo Pivaral <39424641+Epivaral@users.noreply.github.com>
2024-06-18 16:15:57 +0000

    modifying icons table

--- database/icons.sql
+++ database/icons.sql
@@ -6,7 +6,7 @@ CREATE TABLE weather.icons (
     Description_English VARCHAR(50),
     Description_Spanish VARCHAR(50),
     Image VARCHAR(50),
-    Start_Color VARCHAR(7) NOT NULL, -- Hex color code for start of gradient
-    End_Color VARCHAR(7) NOT NULL,   -- Hex color code for end of gradient
+    Start_Color VARCHAR(7) NULL, -- Hex color code for start of gradient
+    End_Color VARCHAR(7) NULL,   -- Hex color code for end of gradient
     PRIMARY KEY (ID, Icon)
 );
