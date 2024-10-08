fd630b5d23ea0b293c4cef6164079ec271b67ec9
Eduardo Pivaral <39424641+Epivaral@users.noreply.github.com>
2024-10-08 18:00:57 +0000

    delete procedure

---
+++ database/delete_weather.sql
@@ -0,0 +1,25 @@
+
+/*
+Description: This stored procedure deletes weather data that is older than 7 days from the weather.WeatherData table.
+
+Procedure: weather.Delete_weather
+Parameters: None
+Returns: None
+
+Usage:
+EXEC weather.Delete_weather;
+
+Notes:
+    - The procedure uses the DATEADD function to calculate the date 7 days before the current date.
+    - The SET NOCOUNT ON statement is used to prevent the message indicating 
+      the number of rows affected by a T-SQL statement from being returned.
+*/
+
+CREATE PROCEDURE weather.Delete_weather()
+AS
+BEGIN
+    SET NOCOUNT ON;
+    DELETE FROM weather.WeatherData
+    WHERE Date_gt < DATEADD(DAY, -7, GETDATE());
+END;
+GO
