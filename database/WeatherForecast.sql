23a0ff8b2c2a96af8b6bcdc46e06ac671672bab9
Eduardo Pivaral <39424641+Epivaral@users.noreply.github.com>
2025-05-17 19:06:24 +0000

    new table

---
+++ database/WeatherForecast.sql
@@ -0,0 +1,42 @@
+CREATE TABLE WeatherForecast (
+    CityCode CHAR(3) NOT NULL,
+    ForecastDate DATETIMEOFFSET,
+    EffectiveDate DATETIMEOFFSET,
+    Quarter INT,
+    IconPhrase NVARCHAR(100),
+    Phrase NVARCHAR(100),
+    
+    TemperatureMin FLOAT,
+    TemperatureMax FLOAT,
+    TemperatureAvg FLOAT,
+    
+    RealFeelMin FLOAT,
+    RealFeelMax FLOAT,
+    RealFeelAvg FLOAT,
+    
+    DewPoint FLOAT,
+    RelativeHumidity INT,
+    
+    WindDirectionDegrees FLOAT,
+    WindDirectionDescription NVARCHAR(10),
+    WindSpeed FLOAT,
+    
+    WindGustDirectionDegrees FLOAT,
+    WindGustDirectionDescription NVARCHAR(10),
+    WindGustSpeed FLOAT,
+    
+    Visibility FLOAT,
+    CloudCover INT,
+    
+    HasPrecipitation BIT,
+    PrecipitationType NVARCHAR(20),
+    PrecipitationIntensity NVARCHAR(20),
+    
+    PrecipitationProbability INT,
+    ThunderstormProbability INT,
+    
+    TotalLiquid FLOAT,
+    Rain FLOAT,
+    Snow FLOAT,
+    Ice FLOAT
+);
