ba91241476e97b704c4d9b819b870ec897e857e8
Eduardo Pivaral <39424641+Epivaral@users.noreply.github.com>
2025-07-17 07:34:53 -0600

    Update get_weatherforecast.sql

--- database/get_weatherforecast.sql
+++ database/get_weatherforecast.sql
@@ -1,4 +1,4 @@
-CREATE PROCEDURE weather.GetWeatherForecast
+CREATE PROCEDURE weather.GetWeatherForecast 
  @CityCode CHAR(3)
 AS
 BEGIN
@@ -6,14 +6,15 @@ BEGIN
 
     DROP TABLE IF EXISTS #ForecastMeasures;
 
-    SELECT top (4) *
+    SELECT top (4)
+        *
     into #ForecastMeasures
-   	FROM dbo.WeatherForecast
+    FROM dbo.WeatherForecast
     WHERE CityCode = @CityCode
-   	ORDER BY ForecastDate DESC;
-   
-   	SELECT * FROM 
-   	#ForecastMeasures
-   	ORDER BY EffectiveDate;
+    ORDER BY ForecastDate DESC;
+
+    SELECT *
+    FROM #ForecastMeasures
+    ORDER BY EffectiveDate;
 END;
 GO
