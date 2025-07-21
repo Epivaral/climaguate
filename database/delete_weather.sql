bd18e44b4bd0d9aa969086c49b83ba6c4890a98b
Eduardo Pivaral <39424641+Epivaral@users.noreply.github.com>
2025-07-21 10:04:59 -0600

    Update delete_weather.sql

--- database/delete_weather.sql
+++ database/delete_weather.sql
@@ -1,6 +1,6 @@
 
 /*
-Description: This stored procedure deletes weather data that is older than 7 days from the weather.WeatherData table.
+Description: This stored procedure deletes weather data that is older than 5 days from the weather.WeatherData table.
 
 Procedure: weather.Delete_weather
 Parameters: None
@@ -10,7 +10,7 @@ Usage:
 EXEC weather.Delete_weather;
 
 Notes:
-    - The procedure uses the DATEADD function to calculate the date 7 days before the current date.
+    - The procedure uses the DATEADD function to calculate the date 5 days before the current date.
     - The SET NOCOUNT ON statement is used to prevent the message indicating 
       the number of rows affected by a T-SQL statement from being returned.
 */
@@ -19,7 +19,20 @@ CREATE PROCEDURE weather.Delete_weather
 AS
 BEGIN
     SET NOCOUNT ON;
+    
+    -- Delete from WeatherData
     DELETE FROM weather.WeatherData
-    WHERE Date_gt < DATEADD(DAY, -7, GETDATE());
+    WHERE Date_gt < DATEADD(DAY, -5, GETDATE());
+
+    -- Delete from WeatherForecast
+    DELETE FROM WeatherForecast
+    WHERE ForecastDate < DATEADD(DAY, -5, GETDATE());
+
+    -- Delete from JobRunLock
+    DELETE FROM JobRunLock
+    WHERE RunTimeUtc < DATEADD(DAY, -5, GETDATE());
+
+
 END;
+
 GO
