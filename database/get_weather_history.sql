d0ef5f3b72e17cb2e86753421e5e29ab93335f70
Eduardo Pivaral <epivaral@studyyourdata.com>
2025-09-22 14:08:52 -0600

    feat(WeatherHistory): Enhance weather history display with chart integration and improve data formatting

--- database/get_weather_history.sql
+++ database/get_weather_history.sql
@@ -6,15 +6,21 @@ BEGIN
     SET NOCOUNT ON;
     
     SELECT
-        Date_gt AS CollectionDate,
-        Main_Humidity,
-        isnull(str(Rain_1h,12,2),'0') as Rain_1h,
-        isnull(str(Rain_3h,12,2),'0') as Rain_3h,
-        Main_Temp,
-        Main_Pressure
+        -- Group by hour, create a datetime for the start of each hour
+        DATEADD(hour, DATEDIFF(hour, 0, Date_gt), 0) AS CollectionDate,
+        
+        -- Average all the measures for each hour
+        AVG(CAST(Main_Humidity AS FLOAT)) AS Main_Humidity,
+        isnull(str(AVG(ISNULL(Rain_1h, 0)),12,2),'0') as Rain_1h,
+        isnull(str(AVG(ISNULL(Rain_3h, 0)),12,2),'0') as Rain_3h,
+        AVG(Main_Temp) AS Main_Temp,
+        AVG(CAST(Main_Pressure AS FLOAT)) AS Main_Pressure
     FROM weather.WeatherData
     WHERE CityCode = @CityCode
       AND Date_gt >= DATEADD(day, -@Days, GETDATE())
+    GROUP BY 
+        -- Group by year, month, day, and hour
+        DATEADD(hour, DATEDIFF(hour, 0, Date_gt), 0)
     ORDER BY CollectionDate DESC;
 END;
 GO
