cd06dad88b9cb347c2d601fc25d54fefe9c3f456
Eduardo Pivaral <epivaral@galileo.edu>
2026-06-03 20:04:27 -0600

    improve speed and code cleanup

--- frontend/Client/CitySlugHelper.cs
+++ frontend/Client/CitySlugHelper.cs
@@ -19,4 +19,26 @@ public static class CitySlugHelper
 
     public static string? SlugToCityCode(string slug, IEnumerable<CityEntry> cities) =>
         cities.FirstOrDefault(c => ToSlug(c.CityName) == slug)?.CityCode;
+
+    public static string GetSoilTypeDescription(string soilType) => soilType switch
+    {
+        "Andosol" => "Andosol - Suelos volcánicos muy fértiles",
+        "Fluvisol" => "Fluvisol - Suelos aluviales de valles",
+        "Acrisol"  => "Acrisol - Suelos ácidos tropicales",
+        "Regosol"  => "Regosol - Suelos jóvenes poco desarrollados",
+        "Cambisol" => "Cambisol - Suelos de desarrollo moderado",
+        "Leptosol" => "Leptosol - Suelos delgados sobre roca",
+        "Luvisol"  => "Luvisol - Suelos con acumulación de arcilla",
+        _          => $"{soilType} - Tipo de suelo especializado"
+    };
+
+    public static string GetClimateDescription(string climateZone) => climateZone switch
+    {
+        "Aw"  => "Aw - Sabana tropical (lluvias estacionales)",
+        "Af"  => "Af - Ecuatorial lluvioso (lluvia constante)",
+        "Am"  => "Am - Monzónico tropical (muy lluvioso)",
+        "Cwa" => "Cwa - Subtropical húmedo (invierno seco)",
+        "Cwb" => "Cwb - Oceánico subtropical de altura (templado de montaña)",
+        _     => $"{climateZone} - Zona climática especializada"
+    };
 }
